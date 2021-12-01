6480f7294c15a71df8a81787db8d3dff4818eb9a
Brigitte <brija.sinankwa@gmail.com>
2021-12-01 09:48:21 +0100

    Random "Bicycle"

--- DemoProject/src/Bicycle.java
+++ DemoProject/src/Bicycle.java
@@ -2,11 +2,23 @@ import java.util.Random;
 
 public class Bicycle {
 
-
 	public static void main(String[] args) {
-	
-		String wort = "BYCICLE";
+		Random random = new Random();
+		 
+		String wort = "BICYCLE";
+		char[] buchstaben = wort.toCharArray();
 		
-	}
+		int zufall;
+		
+		for(int i = 0; i < wort.length(); i++) {
+			
+			zufall = random.nextInt(wort.length());
+			char tmp = buchstaben[i];
+			buchstaben[i] = buchstaben[zufall];
+			buchstaben[zufall] = tmp;
+		}
+		
+		System.out.print(new String(buchstaben));
 
+	}
 }
