02c88e5fa759778e4f6705acca3f70ff46390de3
Brigitte <brija.sinankwa@gmail.com>
2021-12-08 09:23:42 +0100

    Check

--- DemoProject/src/Bicycle.java
+++ DemoProject/src/Bicycle.java
@@ -5,26 +5,36 @@ public class Bicycle {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		 
+
 		String wort = "BICYCLE";
 		char[] buchstaben = wort.toCharArray();
-		
+
 		int zufall;
-		
-		for(int i = 0; i < wort.length(); i++) {
-			
+
+		for (int i = 0; i < wort.length(); i++) {
+
 			zufall = random.nextInt(wort.length());
 			char tmp = buchstaben[i];
 			buchstaben[i] = buchstaben[zufall];
 			buchstaben[zufall] = tmp;
 		}
-		
+
 		System.out.println(new String(buchstaben));
-		
+
 		Scanner eingabe = new Scanner(System.in);
 		System.out.print("Dein Tipp: ");
-		String tipp = eingabe.next(); 
-		
+		String tipp = eingabe.next();
+
+		boolean check = tipp.equals(buchstaben);
+
+		if (tipp.equals(buchstaben)) {
+			System.out.println(check);
+		}
+		if (tipp.equals("BICYCLE")) {
+			System.out.println("Richtig!");
+		} else {
+			System.out.println("Versuchen Sie es erneut!");
+		}
 
 	}
 }
